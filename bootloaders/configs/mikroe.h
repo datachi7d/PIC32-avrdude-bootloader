a091b397e578326ea86af7264c8c18e99da16700
Sean Kelly <seandtkelly@gmail.com>
2016-03-26 17:43:49 +1300

    Add mikro-e 6LoWPAN Clicker
    Signed-off-by: Sean Kelly <seandtkelly@gmail.com>

--- bootloaders/configs/mikroe.h
+++ bootloaders/configs/mikroe.h
@@ -1,6 +1,7 @@
 /* MikroE boards */
 
 #define     prodPIC32MXClicker          0x0001
+#define     prod6LOWPANClicker          0x0002
 
 #if defined(_BOARD_MIKROE_PIC32MX_CLICKER_)
 #define _CONFIG_VALID_
@@ -73,3 +74,70 @@
     #define FLASH_PAGE_SIZE             4096
     #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
 #endif
+
+#if defined(_BOARD_MIKROE_6LOWPAN_CLICKER_)
+#define _CONFIG_VALID_
+
+#if defined(PUT_CONFIG_BITS_HERE)
+
+#pragma config FNOSC     = PRIPLL            // Oscillator Selection
+#pragma config FPLLIDIV  = DIV_2             // PLL Input Divider (PIC32 Starter Kit: use divide by 2 only)
+#pragma config FPLLMUL   = MUL_20            // PLL Multiplier
+#pragma config FPLLODIV  = DIV_1             // PLL Output Divider
+#pragma config UPLLIDIV = DIV_2         // USB PLL Input Divider (2x Divider)
+#pragma config UPLLEN = ON              // USB PLL Enable (Enabled)
+#pragma config FPBDIV    = DIV_1             // Peripheral Clock divisor
+
+#pragma config FWDTEN    = OFF               // Watchdog Timer
+#pragma config WDTPS     = PS32768           // Watchdog Timer Postscale
+#pragma config FCKSM     = CSDCMD            // Clock Switching & Fail Safe Clock Monitor
+#pragma config OSCIOFNC  = OFF               // CLKO Enable
+#pragma config POSCMOD   = HS                // Primary Oscillator
+#pragma config IESO      = OFF               // Internal/External Switch-over
+#pragma config FSOSCEN   = ON               // Secondary Oscillator Enable
+
+#pragma config CP        = OFF               // Code Protect
+#pragma config BWP       = OFF               // Boot Flash Write Protect
+#pragma config PWP       = OFF               // Program Flash Write Protect
+#pragma config ICESEL    = ICS_PGx2          // ICE/ICD Comm Channel Select
+#pragma config DEBUG     = OFF               // Debugger Disabled for Starter Kit
+#pragma config FSRSSEL   = PRIORITY_7
+
+#pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
+#pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
+
+#endif
+
+    #define CAPABILITIES    (blCapBootLED | blCapDownloadLED | blCapUSBInterface | blCapProgramButton | blCapVirtualProgramButton | CAPCOMMON)
+
+    // BTN / LED sense
+    #define LedOn       High
+    #define BntOn       Low
+
+    // Boot LED
+    #define BLedLat     B
+    #define BLedBit     1
+
+    // Data LED
+    #define DLedLat     B
+    #define DLedBit     2
+
+    // Virtual program button
+    #define VPBntLat    E
+    #define VPBntBit    7
+
+    // Program button
+    #define PBntPort    E
+    #define PBntBit     7
+
+    #define _CPU_NAME_                  "32MX470F512H"
+    #define VEND                        vendMikroE
+    #define PROD                        prod6LOWPANClicker
+    #define F_CPU                       80000000UL
+    #define F_PBUS                      F_CPU
+
+    #define FLASH_BYTES                 0x80000                     // 512K
+    #define FLASH_PAGE_SIZE             4096
+    #define LoadFlashWaitStates()       (CHECON = 2)            // 0 for 0-30Mhz, 1 for 31-60Mhz, 2 for 61-80Mhz
+#endif
+
